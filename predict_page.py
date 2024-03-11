8d626e0e1090dd1d00e016079d5e67b6a86b60d8
cayleb247 <calebtw8@gmail.com>
2024-03-11 00:03:35 -0400

    Adding project to GitHub repo

---
+++ predict_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def show_predict_page():
+    st.title("Machine Learning Live Security Surveillance")
+
+    st.write("""### Using ConvLSTM Trained Model to Detect Crime""")
+
